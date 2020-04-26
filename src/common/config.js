389970aa723429f7996f59fc8cbc609ff055ba35
mr-phlames <mickdd22@gmail.com>
2020-04-26 17:12:15 +0000

    added app url to app config

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,6 @@
-// export const API_URL = "http://localhost:7000";
-export const API_URL = "http://myadsman.000webhostapp.com";
+export const APP_URL = window.location.origin;
+export const API_URL = "http://localhost:7000";
+// export const API_URL = "http://myadsman.000webhostapp.com";
 export const USER_STORAGE_KEY = "business.user";
 export const TOKEN_STORAGE_KEY = "token";
 export const MAINTAINANCE_MODE = false;
