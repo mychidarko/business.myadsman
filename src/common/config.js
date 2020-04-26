19b0cc5023734f40afa7fa47c9c0efb0bc3c3b42
mr-phlames <mickdd22@gmail.com>
2020-04-26 03:11:31 +0000

    updated API URL

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 // export const API_URL = "http://localhost:7000";
-export const API_URL = "http://myadsman.000webhostapp.com/";
+export const API_URL = "http://myadsman.000webhostapp.com";
 export const USER_STORAGE_KEY = "business.user";
 export const TOKEN_STORAGE_KEY = "token";
 export const MAINTAINANCE_MODE = false;
