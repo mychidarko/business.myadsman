50b19a6c398c0108116a454b2831c601fd1353ac
mr-phlames <mickdd22@gmail.com>
2020-04-27 02:48:22 +0000

    updated API URL

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,6 @@
 export const APP_URL = window.location.origin;
-export const API_URL = "http://localhost:7000";
-// export const API_URL = "http://myadsman.000webhostapp.com";
+// export const API_URL = "http://localhost:7000";
+export const API_URL = "https://mybitcart.com/adsman";
 export const USER_STORAGE_KEY = "business.user";
 export const TOKEN_STORAGE_KEY = "token";
 export const MAINTAINANCE_MODE = false;
